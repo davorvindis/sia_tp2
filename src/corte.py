c69ddf02f05be5c41f38408e0cbf189a2cb1a393
reurison-sr <43077478+reurison-sr@users.noreply.github.com>
2021-04-03 21:23:13 -0300

    Update corte.py
    Add contenido() and aceptable() cut methods.

--- src/corte.py
+++ src/corte.py
@@ -14,6 +14,31 @@ def corte_wrapper(fn):
             while i > 0:
                 fn()
                 i -= 1
+                
+        if args[0] == "contenido":
+            '''El mejor fitness no cambia en una cantidad de generaciones'''
+            CtdGeneraciones = int(args[1])
+            count = 0
+            while True:
+                bestFitnessOld, bestFitnessNew = fn()
+                delta_Fitness = abs(bestFitnessOld - bestFitnessNew)
+                if (delta_Fitness == 0):
+                    count += 1
+                else:
+                    count = 0
+                if count == CtdGeneraciones:
+                    print("STOP")
+                    break
+                    
+        if args[0] == "aceptable":
+            '''|fitness(k-1) - fitness(k)| < treshold -> STOP'''
+            treshold = args[1]
+            while True:
+                bestFitnessOld, bestFitnessNew = fn()
+                delta_Fitness = abs(bestFitnessOld - bestFitnessNew)
+                if delta_Fitness < treshold:
+                    print("STOP")
+                    break
     return inner1
 
 
@@ -22,8 +47,8 @@ def corte_wrapper(fn):
 
 
 # Criterios de corte (0/5)
-#    * Tiempo
-#    * Cantidad de generaciones
-#    * Solucion aceptable
+#    * Tiempo (ok)
+#    * Cantidad de generaciones (ok)
+#    * Solucion aceptable (ok)
 #    * Estructura
-#    * Contenido
+#    * Contenido (ok)
